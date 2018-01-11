70e1a070691e7261262af7808a9d98519b18b036
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-01-11 14:14:15 -0500

    added fat free project

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    // require autoload file
+    require_once('vendor/autoload.php');
+
+    // create instance of base class
+    $f3 = Base::instance();
+
+    // define a default route
+    $f3->route('GET /', function() {
+            $view = new View;
+            echo $view->render
+            ('views/home.html');
+        }
+    );
+
+    // run fat free
+    $f3->run();
+
+?>
+
+<h3>Hello!</h3>
+
+<p>Making a change to my index page</p>
